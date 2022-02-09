75e6af4f9540a5f04a8a3cec5f0c3a2e2ddc4f58
Beda Vladimir <duracellp@yandex.ru>
2022-02-09 21:24:07 +0300

    Запуск браузера и переход на страницу в Википедии

--- first_test.py
+++ first_test.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 try:
     browser = webdriver.Chrome()
-    browser.get("https://www.google.ru/")
-    button = browser.find_element(By.CSS_SELECTOR, ".FPdoLc.lJ9FBc .RNmpXc").click()
+    browser.get("https://en.wikipedia.org/wiki/Main_Page")
+    search = browser.find_element(By.CLASS_NAME, "vector-search-box-input").send_keys("Selenium")
+    button = browser.find_element(By.CSS_SELECTOR, "#searchButton").click()
 finally:
     browser.quit()
